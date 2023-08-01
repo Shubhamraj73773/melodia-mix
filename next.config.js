3f2500c9400fdf22755d5d82734b824112856a7e
Shubham Raj <shubhamraj73773@gmail.com>
2023-08-01 15:19:34 +0530

    melodia-mix

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            "xttokxzhwrvmcmcctgvs.supabase.co"
+        ]
+    }
+}
+
+module.exports = nextConfig
